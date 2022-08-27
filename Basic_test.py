d6a478458c322cf8553f7ca9f270205200f42e11
mmalek7 <111284272+mmalek7@users.noreply.github.com>
2022-08-27 07:45:29 +0200

    fifth

--- Basic_test.py
+++ Basic_test.py
@@ -1,8 +1,13 @@
-x = 41
+# x = 41
+#
+# if x > 10:
+#   print("Above ten,")
+#   if x > 20:
+#     print("and also above 20!")
+#   else:
+#     print("but not above 20.")
 
-if x > 10:
-  print("Above ten,")
-  if x > 20:
-    print("and also above 20!")
-  else:
-    print("but not above 20.")
+i=1
+while i<6:
+    print(i)
+    i+=1
