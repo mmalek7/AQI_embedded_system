c650c89e4421a8f7a6d79d12470046a4f2541c37
mmalek7 <111284272+mmalek7@users.noreply.github.com>
2022-08-29 09:08:30 +0200

    fifth

--- Basic_test.py
+++ Basic_test.py
@@ -7,7 +7,13 @@
 #   else:
 #     print("but not above 20.")
 
-i=1
-while i<100:
-    print(i)
-    i+=1
+# i=1
+# while i<100:
+#     print(i)
+#     i+=1
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
